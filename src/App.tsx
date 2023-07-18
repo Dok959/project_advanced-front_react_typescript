19a45ca2b52a7eefd1d4b952a2704445c84589a8
Dok959 <dok959.developer@gmail.com>
2023-07-18 16:25:59 +0400

    Добавлена работа с темами, настроено их переключение

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import AboutPage from './pages/AboutPage/AboutPage';
 import MainPage from './pages/MainPage/MainPage';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { Suspense } from 'react';
+import { Suspense, useContext, useState } from 'react';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading ...</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, useMemo, useState } from 'react';
+import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from './ThemeContext';
+
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(
+    () => ({
+      theme: theme,
+      setTheme: setTheme,
+    }),
+    [theme],
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
