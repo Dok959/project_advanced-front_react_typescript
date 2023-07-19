4555bd02af16a054df61cf4ac61854d9ec60df93
Dok959 <dok959.developer@gmail.com>
2023-07-19 10:50:41 +0400

    Создан хелпер для работы с классами

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense, useContext, useState } from 'react';
 import { Theme, ThemeContext } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
