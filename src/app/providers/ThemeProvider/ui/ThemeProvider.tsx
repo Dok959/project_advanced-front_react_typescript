f7b4edc3f4d928c5016f653d8a4624213bd4909f
Dok959 <dok959.developer@gmail.com>
2023-07-19 11:58:01 +0400

    Структура приведена в соответствие с FSD, настроены абсолютные пути

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,9 @@
 import { ReactNode, useMemo, useState } from 'react';
-import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from './ThemeContext';
+import {
+  ThemeContext,
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+} from '../lib/ThemeContext';
 
 const defaultTheme =
   (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
