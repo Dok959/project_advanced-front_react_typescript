fc71624468b0a359650915eb75c0b3cd85f8337c
Dok959 <dok959.developer@gmail.com>
2023-07-27 11:22:49 +0400

    Добавлены линтеры для css, проведено форматирование

--- src/widgets/LangSwitcher/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -1,28 +1,27 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './LangSwitcher.module.scss';
 import { useTranslation } from 'react-i18next';
 import { Button, ThemeButton } from '../../shared/ui/Button/Button';
 
 interface LangSwitcherProps {
-  className?: string;
+    className?: string;
 }
 
-export const LangSwitcher = ({ className }: LangSwitcherProps) => {
-  const { t, i18n } = useTranslation();
+export const LangSwitcher = ({
+    className = '',
+}: LangSwitcherProps): JSX.Element => {
+    const { t, i18n } = useTranslation();
 
-  const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-  };
+    const toggle = (): void => {
+        void i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    };
 
-  return (
-    <>
-      <Button
-        theme={ThemeButton.CLEAR}
-        onClick={toggle}
-        className={classNames(cls.LangSwitcher, {}, [className])}
-      >
-        {t('Язык')}
-      </Button>
-    </>
-  );
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+            className={classNames('', {}, [className])}
+        >
+            {t('Язык')}
+        </Button>
+    );
 };
