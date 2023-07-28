33542231b7b6364f25d2f89b1bbfdaecad759f55
Dok959 <dok959.developer@gmail.com>
2023-07-28 08:14:07 +0400

    Добавлен модуль анализа размера бандла

--- config/build/buildPlagins.ts
+++ config/build/buildPlagins.ts
@@ -1,26 +1,30 @@
-import webpack from 'webpack'
-import HTMLWebpackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { type BuildOptions } from './types/config'
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { type BuildOptions } from './types/config';
 // import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export function buildPlagins ({
-  paths,
-  isDev
+export function buildPlagins({
+    paths,
+    isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HTMLWebpackPlugin({
-      template: paths.html
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
-    }),
-    new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
-    }),
-    // new ReactRefreshWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        // new ReactRefreshWebpackPlugin(),
+        // new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
+    ];
 }
